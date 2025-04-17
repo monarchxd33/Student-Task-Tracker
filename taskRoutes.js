3956438f73d3b699c0e8f21d5e5e8e73be6f3dbb
Ayush Rana <124562908+monarchxd33@users.noreply.github.com>
2025-04-17 19:24:20 +0530

    Delete taskRoutes.js

--- taskRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAllTasks, createTask } from '../controllers/taskController.js';
-
-const router = express.Router();
-
-router.get('/', getAllTasks);
-router.post('/', createTask);
-
-export default router;
